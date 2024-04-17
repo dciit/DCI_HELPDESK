fbf0787c929f46682b5a1b0e51f8f831510bbcb5
github-devmaker <peerapong.kiadtipongwiriya@gmail.com>
2024-04-17 11:07:51 +0700

    update commit

--- src/pages/sparepart.tsx
+++ src/pages/sparepart.tsx
@@ -10,136 +10,113 @@ import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import ButtonGroup from "@mui/material/ButtonGroup";
 
 function Sparepart() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = 3;
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = 3;
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    console.log(activeStep);
-  };
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        console.log(activeStep);
+    };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    console.log(activeStep);
-  };
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+        console.log(activeStep);
+    };
 
-  //   useEffect(() => {
-  //    alert(activeStep);
-
-  //    if(activeStep == 0){
-
-  //    }
-  //    else if(activeStep == 1){
-
-  //    }else{
-
-  //    }
-
-  //   }, [activeStep]);
-
-  //   const Nextpage = (_step:number) => {
-  //     let step = activeStep;
-  //     return (
-  //       <>
-
-  //       </>
-  //     );
-  //   };
-
-  function getGreeting(_step: number) {
-    if (_step == 0) {
-      return (
-        <Box
-          sx={{
-            display: "flex",
-            "& > *": {
-              m: 1,
-            },
-          }}
-        >
-          <ButtonGroup
-            orientation="vertical"
-            aria-label="Vertical button group"
-            variant="contained"
-          >
-            <Button key="one">One</Button>,<Button key="two">Two</Button>,
-            <Button key="three">Three</Button>,
-          </ButtonGroup>
-        </Box>
-      );
-    } else if (_step == 1) {
-      return <h1>STEP2</h1>;
-    } else {
-      return <h1>STEP3</h1>;
+    function getGreeting(_step: number) {
+        if (_step == 0) {
+            return (
+                <Box
+                    sx={{
+                        display: "flex",
+                        "& > *": {
+                            m: 1,
+                        },
+                    }}
+                >
+                    <ButtonGroup
+                        orientation="vertical"
+                        aria-label="Vertical button group"
+                        variant="contained"
+                    >
+                        <Button key="one">One</Button>,<Button key="two">Two</Button>,
+                        <Button key="three">Three</Button>,
+                    </ButtonGroup>
+                </Box>
+            );
+        } else if (_step == 1) {
+            return <h1>STEP2</h1>;
+        } else {
+            return <h1>STEP3</h1>;
+        }
     }
-  }
 
-  return (
-    <>
-      <div className="bg-auto bg-no-repeat bg-center">
-        {/* <Box sx={{ maxWidth: 500, flexGrow: 1 }}> */}
-        <Paper
-          square
-          elevation={0}
-          sx={{
-            textAlign: "center",
-            fontSize: "h6.fontSize",
-            fontFamily: "Monospace",
-          }}
-        >
-          <Typography
-            sx={{
-              textAlign: "center",
-              fontSize: "h5.fontSize",
-              background: "#1976d2",
-              color: "white",
-            }}
-          >
-            เบิกอุปกรณ์ IT
-          </Typography>
-        </Paper>
+    return (
+        <>
+            <div className="bg-auto bg-no-repeat bg-center">
+                {/* <Box sx={{ maxWidth: 500, flexGrow: 1 }}> */}
+                <Paper
+                    square
+                    elevation={0}
+                    sx={{
+                        textAlign: "center",
+                        fontSize: "h6.fontSize",
+                        fontFamily: "Monospace",
+                    }}
+                >
+                    <Typography
+                        sx={{
+                            textAlign: "center",
+                            fontSize: "h5.fontSize",
+                            background: "#1976d2",
+                            color: "white",
+                        }}
+                    >
+                        เบิกอุปกรณ์ IT
+                    </Typography>
+                </Paper>
 
-        <div>{getGreeting(activeStep)}</div>
+                <div>{getGreeting(activeStep)}</div>
 
-        <MobileStepper
-          variant="text"
-          steps={maxSteps}
-          position="bottom"
-          activeStep={activeStep}
-          nextButton={
-            <Button
-              size="medium"
-              onClick={handleNext}
-              disabled={activeStep === maxSteps - 1}
-            >
-              Next
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={handleBack}
-              disabled={activeStep === 0}
-            >
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowRight />
-              ) : (
-                <KeyboardArrowLeft />
-              )}
-              Back
-            </Button>
-          }
-        />
-        {/* </Box> */}
-      </div>
-    </>
-  );
+                <MobileStepper
+                    variant="text"
+                    steps={maxSteps}
+                    position="bottom"
+                    activeStep={activeStep}
+                    nextButton={
+                        <Button
+                            size="medium"
+                            onClick={handleNext}
+                            disabled={activeStep === maxSteps - 1}
+                        >
+                            Next
+                            {theme.direction === "rtl" ? (
+                                <KeyboardArrowLeft />
+                            ) : (
+                                <KeyboardArrowRight />
+                            )}
+                        </Button>
+                    }
+                    backButton={
+                        <Button
+                            size="small"
+                            onClick={handleBack}
+                            disabled={activeStep === 0}
+                        >
+                            {theme.direction === "rtl" ? (
+                                <KeyboardArrowRight />
+                            ) : (
+                                <KeyboardArrowLeft />
+                            )}
+                            Back
+                        </Button>
+                    }
+                />
+                {/* </Box> */}
+            </div>
+        </>
+    );
 }
 
 export default Sparepart;
