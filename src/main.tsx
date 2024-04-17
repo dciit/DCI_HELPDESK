a931cbefedd0ebf40bd9a9bf13ed92632e3feb97
github-devmaker <peerapong.kiadtipongwiriya@gmail.com>
2024-04-17 14:43:50 +0700

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,48 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {
-  BrowserRouter,
-  Route,
-  Routes,
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
+import Login from './pages/login.tsx';
 import Workorder from './pages/workorder.tsx';
 import Sparepart from './pages/sparepart.tsx';
 import NotFound from './pages/notfound.tsx';
-import Layout from './pages/layout.tsx';
-import Account from './pages/account.tsx';
 import { MContext } from './interface.ts';
-// import { MContext, MContextWorkorder } from './interface.ts';
 const BASE = import.meta.env.VITE_PATH;
 
+const ThemeContext = createContext<MContext>({});
 const context: MContext = {
-  appname: 'IT HELPDESK',
-  themelight: {
-    bgColor: 'white',
-    textTitleColor: 'text-[#049EF4]',
-    textContentColor: 'text-[#6d6d6d]',
-  }
+  appname: 'IT HELPDESK'
 }
-export const ThemeContext = createContext<MContext>({});
+const router = createBrowserRouter([
+  {
+    path: `/${BASE}/`,
+    element: <Login />,
+  },
+  {
+    path: `/${BASE}/*`,
+    element: <NotFound />
+  },
+  {
+    path: `/${BASE}/login`,
+    element: <Login />
+  },
+  {
+    path: `/${BASE}/workorder`,
+    element: <Workorder />
+  },
+  {
+    path: `/${BASE}/sparepath`,
+    element: <Sparepart />
+  }
+
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeContext.Provider value={context}>
-      <BrowserRouter>
-        <Routes >
-          <Route element={<Layout />}>
-            <Route path={`${BASE}/*`} element={<NotFound />} />
-            <Route path={`${BASE}/workorder`} element={<Workorder />} />
-            <Route path={`${BASE}/sparepart`} element={<Sparepart />} />
-            <Route path={`${BASE}/account`} element={<Account />} />
-          </Route>
-          <Route path={`${BASE}`} element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeContext.Provider>
   </React.StrictMode>,
 )
